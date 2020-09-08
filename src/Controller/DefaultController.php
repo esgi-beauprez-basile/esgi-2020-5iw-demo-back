74789bd878deb08dc0f59158cc19dfc27b9b16bc
Bbeauprez <basile.beauprez.59@gmail.com>
2020-09-08 15:34:08 +0200

    update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,7 +13,19 @@ class DefaultController
         return new JsonResponse([
             [
                 'id' => 1,
-                'title' => 'test 12',
+                'title' => 'test inutile',
+                'completed' => false,
+            ], [
+                'id' => 2,
+                'title' => 'test utile',
+                'completed' => false,
+            ], [
+                'id' => 3,
+                'title' => 'je m\'ennuie',
+                'completed' => false,
+            ], [
+                'id' => 4,
+                'title' => 'c\'est la pôse',
                 'completed' => false,
             ],
         ], 200, ['Access-Control-Allow-Origin' => '*']);
